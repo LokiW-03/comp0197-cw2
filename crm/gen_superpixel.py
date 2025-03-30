8237211f5262e13156401f1d1e4918c6562d89df
Lok <loki3003wong@gmail.com>
2025-03-30 00:54:07 +0000

    remove extra package

--- crm/gen_superpixel.py
+++ crm/gen_superpixel.py
@@ -3,7 +3,6 @@ import torch
 from skimage.segmentation import slic
 from skimage.color import rgb2lab
 from glob import glob
-from tqdm import tqdm
 import numpy as np
 from PIL import Image
 
@@ -11,7 +10,8 @@ def generate_superpixels(image_dir, save_dir, n_segments=100, compactness=10):
     os.makedirs(save_dir, exist_ok=True)
     image_paths = glob(os.path.join(image_dir, "*.jpg"))
 
-    for path in tqdm(image_paths, desc="Generating SLIC superpixels"):
+    for path in image_paths:
+        print(f"Processing: {os.path.basename(path)}")
         image = Image.open(path).convert("RGB")
         image = np.array(image)
         lab_img = rgb2lab(image)  # improves segmentation quality
