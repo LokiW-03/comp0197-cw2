1cdbab7129bd0cf73042cb0dc53995ec7f3b2f77
Jesslzy <126081495+Jesslzy@users.noreply.github.com>
2025-04-12 17:42:37 +0100

    refactor: remove unused constants in crm

--- crm/constants.py
+++ crm/constants.py
@@ -5,6 +5,3 @@ NUM_EPOCHS = 10
 CRM_MODEL_SAVE_PATH = "crm_models"
 CLS_LR = 1e-3
 REC_LR = 1e-2
-
-RESNET_PATH = "cam/saved_models/resnet50_pet_cam.pth"
-EFFNET_PATH = "cam/saved_models/efficientnet_pet_scorecam.pth"
